54d5fb98549593e1b1bcced644755ae1db430969
Tojumi1234 <tojumiakanimodo@gmail.com>
2024-08-30 13:15:53 +0300

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,66 @@
+// javascript communication
+
+document.write("This is Javascript"); // communication with html file
+console.log("Hello World!"); // communication with browser console
+window.alert("Hello worldpop!");        // communication using Alert dialog box
+
+// variables
+
+/*
+Multiple line comment
+var
+const
+let
+*/
+
+// Declaring variables
+var x = 7;
+document.write(x);
+console.log(x);
+window.alert(x);
+
+//Case Sensitive
+
+var Cars = "toyota";
+var cars = "toyota";
+var nameCars = "toyota";
+var NameCars = "toyota";
+console.log(Cars);
+console.log(nameCars);
+console.log(cars);
+console.log(NameCars);
+
+// Data Types
+
+/*
+Number
+String
+Boolean
+Null
+Undefined
+Object
+*/
+
+// Number
+var int=1200;
+var float = 21.02;
+console.log(int);
+console.log(float);
+
+// String
+var CarsName = "BMW"
+var CarsName2 = "Range Rover"
+console.log(CarsName);
+console.log(CarsName3);
+
+// Boolean
+var MyCar = true;
+var MyCar = false;
+document.write(MyCar);
+
+// Null and undefined
+var myMy = null;
+var myword= undefined;
+
+console.log(myMy);
+console.log(myword);
